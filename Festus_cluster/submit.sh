8a90acfd416221761b184e2f1a4a02ccd84ea641
Aritro Chattopadhyay <aritro.c@proton.me>
2026-06-01 17:18:59 +0200

    Add SLURM job submission script

---
+++ Festus_cluster/submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="my_job"
+#SBATCH -n 128
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=user@mail.com
+#SBATCH --time=24:00:00
+#SBATCH --error=%x_%j.err
+#SBATCH --output=%x_%j.out
+
+mpirun -np 32 ./stagyympi
